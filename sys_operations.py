b44bd83b366774c8a0b2f0a28be56403d6f4252c
Leonard <101511592@georgebrown.ca>
2025-03-04 10:45:54 -0500

    added Operating System Name

---
+++ sys_operations.py
@@ -0,0 +1,27 @@
+import platform
+import socket
+import os
+
+print("Current machine type")
+print(platform.machine()) # Apple M1, M2, M3, M4, Raspberry Pi : arm64; for intel/AMD: x86
+print ("-------------------------------\n")
+
+print("Current Processor Type:")
+print(platform.architecture())
+print ("-------------------------------\n")
+
+print("Set timeout of socket in seconds")
+socket.setdefaulttimeout(50)
+print ("-------------------------------\n")
+
+print("Get timeout of socket in seconds")
+socket.getdefaulttimeout()
+print ("-------------------------------\n")
+
+print("Operating System type")
+print(os.name)
+print ("-------------------------------\n")
+
+print("Set timeout of socket in seconds")
+print(platform.system())
+print ("-------------------------------\n")
