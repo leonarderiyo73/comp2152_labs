3403d8c37e0fadc03ccb048feed89a801bddfc61
Leonard <101511592@georgebrown.ca>
2025-03-25 10:55:12 -0400

    New commits

--- Week12/tick.py
+++ Week12/tick.py
@@ -1,3 +1,10 @@
 # Tick class for aggregation
 class Tick:
-    pass
+    def __init__(self):
+        print("Aggregation:Tick is created.")
+
+    def consume_blood(self):
+        print("Aggregation:Tick is consuming blood.")
+
+    def __del__(self):
+        print("Aggregation:Tick is destroyed.")
